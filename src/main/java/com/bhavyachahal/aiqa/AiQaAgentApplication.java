a1c07ff0731064dd7c95518982e10e40d2d9b0ef
Bhavya Chahal <bhayva59chahal@gmail.com>
2026-09-07 21:27:23 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/bhavyachahal/aiqa/AiQaAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bhavyachahal.aiqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiQaAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiQaAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhavyachahal/aiqa/AiQaAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavyachahal.aiqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiQaAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
